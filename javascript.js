73062e0f22f8842fa99c0d0542df6a9edbd74efb
mayankkeshil <mkeshil@protonmail.com>
2025-08-16 15:02:25 +0100

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,15 @@
+const menuButton = document.getElementById("menu");
+const menuView = document.getElementById("menu-view");
+const menuViewHeight = menuView.offsetHeight;
+const welcomeSection = document.getElementById("welcome-section");
+menuView.style.display = "none"; 
+clicked = false;
+
+menuButton.addEventListener("click", () => {
+    clicked = !clicked;
+    const menuLinks = clicked ? 
+      (menuView.style.display = "flex",
+       welcomeSection.style.marginTop = `calc(175px + 20px + ${menuViewHeight}px`) :
+      (menuView.style.display = "none",
+       welcomeSection.style.marginTop = `175px`)  ; 
+});
