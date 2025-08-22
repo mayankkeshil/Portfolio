6ffd2e5d36b11cea55025f31ba6ffcc822c69897
mayankkeshil <mkeshil@protonmail.com>
2025-08-22 14:59:13 +0100

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -9,7 +9,7 @@ menuButton.addEventListener("click", () => {
     clicked = !clicked;
     const menuLinks = clicked ? 
       (menuView.style.display = "flex",
-       welcomeSection.style.marginTop = `calc(175px + 20px + ${menuViewHeight}px`) :
+       welcomeSection.style.marginTop = `calc(175px + 20px + ${menuViewHeight}px)`) :
       (menuView.style.display = "none",
        welcomeSection.style.marginTop = `175px`)  ; 
 });
